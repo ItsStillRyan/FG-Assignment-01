49ede15588f531b41c9369d7605c62f19fbc7935
ItsStillRyan <Ryan_Tan_1998@hotmail.sg>
2021-02-04 08:34:57 +0000

    completed base math-op page, all present buttons working, added imagemapresizer

---
+++ Scripts/index.js
@@ -0,0 +1,13 @@
+//call button
+document.querySelector('#playBTN').addEventListener('click', function(){
+
+    document.querySelector('.main-menu-screen').style.display = "none"
+    document.querySelector('.math-op-screen').style.display = "block"
+})
+
+
+
+
+//map resizer
+imageMapResize();
+
