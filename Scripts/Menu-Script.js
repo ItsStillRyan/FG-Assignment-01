6b1177783d0aec5f977204548f146396b6413fd0
ItsStillRyan <Ryan_Tan_1998@hotmail.sg>
2021-02-06 15:26:25 +0000

    added functional coding in menu page, added settings page BG with trigger via button

--- Scripts/Menu-Script.js
+++ Scripts/Menu-Script.js
@@ -1,19 +1,25 @@
+let settingsscreen = "url('../../Images/Menu/Settings-BG.jpg')"
+function screenChange(BG){
+    document.body.style.backgroundImage = BG
+}
+function displayToggle(screen1,screen2){
+    document.querySelector(screen1).style.cssText = `display: none;`
+    document.querySelector(screen2).style.cssText = `display: block`
+}
+
 //Main Menu
 //Play button
 document.querySelector('#playBTN').addEventListener('click', function () {
-    //menu page
-    document.querySelector('.main-menu-screen').style.cssText = `display: none;`
-    //math op screen
-    document.querySelector('.math-op-screen').style.cssText =
-        `display: block;`
-
+    displayToggle('.main-menu-screen', '.math-op-screen')
     imageMapResize();
 })
 
 
-
-
-
+//settings button
+document.querySelector('#settingsBTN').addEventListener('click', function(){
+    displayToggle('.main-menu-screen', '.settings-screen')
+    screenChange(settingsscreen)
+})
 
 
 
