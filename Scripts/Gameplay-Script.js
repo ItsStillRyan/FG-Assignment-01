e30022b5f61b012d5a56edbe967b79e103ba29c7
ItsStillRyan <Ryan_Tan_1998@hotmail.sg>
2021-02-06 12:43:00 +0000

    added gameover screen with scoreboard, triggered via another timeout() function

--- Scripts/Gameplay-Script.js
+++ Scripts/Gameplay-Script.js
@@ -1,33 +1,41 @@
 //Background Changer
 let pausescreen = "url('../../Images/Gameplay/pause-BG.jpg')"
 let timesupscreen = "url('../../Images/Gameplay/timesup-BG.jpg')"
+let gameoverscreen = "url('../../Images/Menu/main-BG.jpg')"
 
 function screenChange(BG){
     document.body.style.backgroundImage = BG
 }
 //Background Changer END
 
-//Settings
-document.querySelector('#pauseBtn').addEventListener('click', function()
-{
-    document.querySelector('.main-gameplay').style.cssText = `display: none;`
-    
-    document.querySelector('.pause-screen').style.cssText = `display: block`
-
-    pauseBG()
-})
-//Settings END
-
-//Times up screen
+function displayToggle(screen1,screen2){
+    document.querySelector(screen1).style.cssText = `display: none;`
+    document.querySelector(screen2).style.cssText = `display: block`
+}
+function pauseBG(){
+    screenChange(pausescreen)
+    displayToggle('.main-gameplay','.pause-screen')
+}
 function timesUpScreen(){
     screenChange(timesupscreen)
-
-    document.querySelector('.main-gameplay').style.cssText = `display: none;`
-
-    document.querySelector('.timesup-screen').style.cssText = `display: block`
-
+    displayToggle('.main-gameplay','.timesup-screen')
+}
+function gameoverScreen(){
+    screenChange(gameoverscreen)
+    displayToggle('.timesup-screen','.gameover-screen')
 }
 
+
+document.querySelector('#pauseBtn').addEventListener('click', function()
+{
+    pauseBG()
+})
+
+
+//Times up screen
 window.setTimeout(timesUpScreen, 3000)
+//Times Up screen END
 
-
+//Final Score
+window.setTimeout(gameoverScreen, 6000)
+//Final Score END
