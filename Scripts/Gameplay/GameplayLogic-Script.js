a416e8767840baf9bbf7d670a47eb5c5edf3235a
ItsStillRyan <Ryan_Tan_1998@hotmail.sg>
2021-02-09 08:01:55 +0000

    added base logic coding for equation in gameplay

--- Scripts/Gameplay/GameplayLogic-Script.js
+++ Scripts/Gameplay/GameplayLogic-Script.js
@@ -4,6 +4,12 @@ function numberGen(maxNum){
     return x
 }
 
+function placementGen(){
+    let x = numberGen(4)
+    let queryClass = '.buttonNum'+ x
+    console.log(queryClass)
+}
+
 
 //MAIN TIMER
 let startcount = 60;
@@ -20,17 +26,23 @@ let counter = setInterval(function(){
 
 
 
+//Option stones
 let x = document.querySelectorAll('.button-option').length
-
-console.log(x)
-
-
 for (let i = 0; i < x; i++){
-    document.querySelectorAll(".button-option")[i].addEventListener('click', function(){
-        document.querySelector(".buttonNum1").innerHTML = numberGen(20)
-        document.querySelector(".buttonNum2").innerHTML = numberGen(20)
-        document.querySelector(".buttonNum3").innerHTML = numberGen(20)
-        document.querySelector(".buttonNum4").innerHTML = numberGen(20)
+    document.querySelectorAll('.button-option')[i].addEventListener('click', function(){
+        //numbers in button changer
+        document.querySelector('.buttonNum1').innerHTML = numberGen(20)
+        document.querySelector('.buttonNum2').innerHTML = numberGen(20)
+        document.querySelector('.buttonNum3').innerHTML = numberGen(20)
+        document.querySelector('.buttonNum4').innerHTML = numberGen(20)
+
+        //equation changer
+        let eqNum1 = document.querySelector('#eqNum1').innerHTML = numberGen(10)
+        let eqNum2 = document.querySelector('#eqNum2').innerHTML = numberGen(10)
+        console.log(eqNum1,eqNum2)
+        let finalAns = eqNum1 + eqNum2
+        console.log(finalAns)
+        // document.getElementsByClassName(placementGen()).innerHTML = finalAns
     })
 }
 
