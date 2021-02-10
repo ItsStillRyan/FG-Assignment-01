f746da461f3ba647222387dd4adb78592e9f52d7
ItsStillRyan <Ryan_Tan_1998@hotmail.sg>
2021-02-10 06:26:54 +0000

    staged highscore counter in subtraction js

--- Scripts/Gameplay/Gameplay-split-js/Gameplay-Subtraction.js
+++ Scripts/Gameplay/Gameplay-split-js/Gameplay-Subtraction.js
@@ -1,40 +1,57 @@
 //Functions
-function numberGen(maxNum){
+
+function numberGen(maxNum) {
     let x = Math.floor((Math.random() * maxNum) + 1);
     return x
 }
-function placementGen(){
-    let x = numberGen(4)
-    let queryClass = ".buttonNum"+ x 
+
+function place(){
+    let y = numberGen(4)
+    return y
+}
+function placementGen() {
+    let queryClass = ".buttonNum" + place()
     return queryClass
+}
+function btnTriggerValue() {
+    let queryClass = ".btnTrig" + place()
+    return queryClass
+}
+
+function positionCatch(){
+    let btnValue = document.querySelector(".btnTrig2").value
+    console.log(btnValue)
+    
+}
+
+
+function randomBetween(min, max){
+  return Math.random() * (max - min) + min;
 }
 
 //Option stones
 let x = document.querySelectorAll('.button-option').length
-for (let i = 0; i < x; i++){
-    document.querySelectorAll('.button-option')[i].addEventListener('click', function(){
+for (let i = 0; i < x; i++) {
+    document.querySelectorAll('.button-option')[i].addEventListener('click', function () {
         //numbers in button changer
         //numbergen + numbergen to shuffle number twice and prevent repeating numbers
-        document.querySelector('.buttonNum1').innerHTML = numberGen(20) + numberGen(5)
-        document.querySelector('.buttonNum2').innerHTML = numberGen(20) - numberGen(5)
-        document.querySelector('.buttonNum3').innerHTML = numberGen(20) + numberGen(5)
-        document.querySelector('.buttonNum4').innerHTML = numberGen(20) - numberGen(5)
+        document.querySelector('.buttonNum1').innerHTML = numberGen(numberGen(30)) 
+        document.querySelector('.buttonNum2').innerHTML = parseInt(randomBetween(6, 31)) - numberGen(5)
+        document.querySelector('.buttonNum3').innerHTML = numberGen(numberGen(30))
+        document.querySelector('.buttonNum4').innerHTML = parseInt(randomBetween(8, 31)) - numberGen(7)
 
-        //equation changer
-        let eqNum1 = numberGen(20)
-        let eqNum2 = numberGen(20)
-            if(eqNum1 > eqNum2){
-                console.log(eqNum1, eqNum2)
-                // document.querySelector('#eqNum1').innerHTML = eqNum1
-                // document.querySelector('#eqNum2').innerHTML = eqNum2
+        //Equation
+        let eqNum1 = parseInt(randomBetween(15, 30))
+        let eqNum2 = parseInt(randomBetween(1, 15))
+        document.querySelector('#eqNum1').innerHTML = eqNum1
+        document.querySelector('#eqNum2').innerHTML = eqNum2
 
-                let finalAns = eqNum1 - eqNum2
-                let placement = placementGen()
-                document.querySelector(placement).innerHTML = finalAns
-            }
-    
+        let finalAns = eqNum1 - eqNum2
+        let placement = placementGen()
+        document.querySelector(placement).innerHTML = finalAns
+       
+        //trigger value catch
         
-        //highscore
-    //    let x = document.querySelector('.button-option').value
     })
 }
+
