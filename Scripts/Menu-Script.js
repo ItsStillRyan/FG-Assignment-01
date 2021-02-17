9b98a84a46fe48a9ca6105e0262ddeb869197940
ItsStillRyan <Ryan_Tan_1998@hotmail.sg>
2021-02-17 14:55:02 +0000

    added axios JSON lookup function for translator, main page sucessfully translated

--- Scripts/Menu-Script.js
+++ Scripts/Menu-Script.js
@@ -1,3 +1,5 @@
+var languageChange = false
+
 let settingsscreen = "url('../../Images/Menu/Settings-BG.jpg')"
 let menuscreen = "url('../../Images/Menu/main-BG.jpg')"
 function screenChange(BG){
@@ -31,5 +33,46 @@ document.querySelector('#back-Btn').addEventListener('click', function(){
 document.querySelector('#highscoreBTN').addEventListener('click', function(){
     document.location.href = '../HTML/Highscore.html'
 })
+
+//english click
+document.querySelector(".englishBtn").addEventListener('click', async function(){
+
+    languageChange = false
+    //axios
+    let response = await axios.get('/JSON/French.json')
+    console.log(response.data.main_menu.play_button)
+
+    let titleFR = document.querySelector("#titlemain") 
+    let playFR = document.querySelector("#playmain") 
+    let OperationsFR = document.querySelector(".math-op") 
+    // let settingsFR = document.querySelector("#titlemain") 
+    let backFR = document.querySelector("#backBtn") 
+    
+    titleFR.src = response.data.main_menu.title
+    playFR.src = response.data.main_menu.play_button
+    OperationsFR.src = response.data.main_menu.operations_select
+    backFR.src = response.data.gameplay.pause_page.back
+})
+//french click
+document.querySelector(".frenchBtn").addEventListener('click', function(){
+    languageChange = true
+})
+
+
+//french swap start
+
+// document.querySelector('.englishBtn').addEventListener('click', async function(){
+//     let response = await axios.get('/JSON/French.json')
+//     console.log(response.data[0].main_menu.title)
+// })
+
+//getting and calling database
+//things to change in main page:
+//title, play button, operations select, settings board, back
+
+// if (languageChange = true){
+
+
+// }
 
 
