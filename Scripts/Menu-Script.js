ee7e0b5c99edbdb0177e2bb9083aea70c9d92a01
ItsStillRyan <Ryan_Tan_1998@hotmail.sg>
2021-02-18 02:27:23 +0000

    streamlining JSON file, staging axios codes for language changer

--- Scripts/Menu-Script.js
+++ Scripts/Menu-Script.js
@@ -1,11 +1,9 @@
-var languageChange = false
-
 let settingsscreen = "url('../../Images/Menu/Settings-BG.jpg')"
 let menuscreen = "url('../../Images/Menu/main-BG.jpg')"
-function screenChange(BG){
+function screenChange(BG) {
     document.body.style.backgroundImage = BG
 }
-function displayToggle(screen1,screen2){
+function displayToggle(screen1, screen2) {
     document.querySelector(screen1).style.cssText = `display: none;`
     document.querySelector(screen2).style.cssText = `display: block`
 }
@@ -19,44 +17,64 @@ document.querySelector('#playBTN').addEventListener('click', function () {
 
 
 //settings transitions 
-document.querySelector('#settingsBTN').addEventListener('click', function(){
+document.querySelector('#settingsBTN').addEventListener('click', function () {
     displayToggle('.main-menu-screen', '.settings-screen')
     screenChange(settingsscreen)
 })
 
-document.querySelector('#back-Btn').addEventListener('click', function(){
+document.querySelector('#back-Btn').addEventListener('click', function () {
     displayToggle('.settings-screen', '.main-menu-screen')
     screenChange(menuscreen)
 })
 
 //highscore transitions
-document.querySelector('#highscoreBTN').addEventListener('click', function(){
+document.querySelector('#highscoreBTN').addEventListener('click', function () {
     document.location.href = '../HTML/Highscore.html'
 })
 
-//english click
-document.querySelector(".englishBtn").addEventListener('click', async function(){
-
-    languageChange = false
-    //axios
+//language clicks
+document.querySelector(".frenchBtn").addEventListener('click', async function () {
+    localStorage.setItem("languageSelected", "FR")
     let response = await axios.get('/JSON/French.json')
     console.log(response.data.main_menu.play_button)
 
-    let titleFR = document.querySelector("#titlemain") 
-    let playFR = document.querySelector("#playmain") 
-    let OperationsFR = document.querySelector(".math-op") 
-    // let settingsFR = document.querySelector("#titlemain") 
-    let backFR = document.querySelector("#backBtn") 
-    
-    titleFR.src = response.data.main_menu.title
-    playFR.src = response.data.main_menu.play_button
-    OperationsFR.src = response.data.main_menu.operations_select
-    backFR.src = response.data.gameplay.pause_page.back
+    let titleFR = document.querySelector("#titlemain")
+    let playFR = document.querySelector("#playmain")
+    let OperationsFR = document.querySelector(".math-op")
+ // let settingsFR = document.querySelector("#titlemain") 
+    let backFR = document.querySelector("#backBtn")
+
+    if (selectedLanguage == "FR") {
+        titleFR.src = response.data.main_menu.title
+        playFR.src = response.data.main_menu.play_button
+        OperationsFR.src = response.data.main_menu.operations_select
+        backFR.src = response.data.gameplay.pause_page.back
+    } else if (selectedLanguage == "EN"){
+        titleFR.src = response.data.main_menu.title
+        playFR.src = response.data.main_menu.play_button
+        OperationsFR.src = response.data.main_menu.operations_select
+        backFR.src = response.data.gameplay.pause_page.back
+    }
+
+
+
+
 })
-//french click
-document.querySelector(".frenchBtn").addEventListener('click', function(){
-    languageChange = true
+document.querySelector(".englishBtn").addEventListener('click', function () {
+    localStorage.setItem("languageSelected", "EN")
 })
+
+
+let selectedLanguage = localStorage.getItem("languageSelected")
+
+async function getData() {
+
+}
+getData()
+
+
+
+
 
 
 //french swap start
